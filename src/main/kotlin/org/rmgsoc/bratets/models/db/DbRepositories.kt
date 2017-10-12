54e47d9dd859770baba0dbbc8277d049351f15d1
KardashovS <kardashovs@tomskasu.ru>
2017-10-12 12:28:51 +0700

    #8 WIP Repositories update, debug data and test query processing

--- src/main/kotlin/org/rmgsoc/bratets/models/db/DbRepositories.kt
+++ src/main/kotlin/org/rmgsoc/bratets/models/db/DbRepositories.kt
@@ -1,6 +1,10 @@
 package org.rmgsoc.bratets.models.db
 
+import org.hibernate.SessionFactory
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+import java.time.Instant
 
 interface BroRepository : CrudRepository<Bro, Long> {
     fun getByTelegramId(telegramId: Long) : Bro?
@@ -8,4 +12,10 @@ interface BroRepository : CrudRepository<Bro, Long> {
 
 interface BroMessageRepository : CrudRepository<BroMessage, Long> {
     fun findFirstByChatIdOrderByTimeDesc(chatId: Long) : BroMessage?
+
+    @Query("SELECT bm FROM BroMessage bm WHERE chatId = :chatId AND time > :startDate AND time < :endDate")
+    fun findMessagesFromPeriodByChatId(
+            @Param("chatId") chatId: Long,
+            @Param("startDate") startDate: Instant,
+            @Param("endDate") endDate: Instant = Instant.now()) : List<BroMessage>
 }
--- src/main/kotlin/org/rmgsoc/bratets/services/modules/BratetsAnalyticsModuleService.kt
+++ src/main/kotlin/org/rmgsoc/bratets/services/modules/BratetsAnalyticsModuleService.kt
@@ -1,14 +1,17 @@
 package org.rmgsoc.bratets.services.modules
 
 import org.rmgsoc.bratets.models.TextMessage
+import org.rmgsoc.bratets.models.db.BroMessageRepository
 import org.rmgsoc.bratets.services.telegram.TelegramConnectorService
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.time.Instant
 import javax.annotation.PostConstruct
 
 @Service
 class BratetsAnalyticsModuleService(
-        val telegramConnectorService: TelegramConnectorService
+        val telegramConnectorService: TelegramConnectorService,
+        val broMessageRepository: BroMessageRepository
 ) : TelegramTextMessageHandler {
     private val logger = LoggerFactory.getLogger(javaClass)
 
@@ -31,6 +34,8 @@ class BratetsAnalyticsModuleService(
     }
 
     override fun processMessage(message: TextMessage) {
+        val messages = broMessageRepository.findMessagesFromPeriodByChatId(-235423713, Instant.parse("2017-10-01T00:00:00.00Z"))
 
+        telegramConnectorService.sendTextMessageToChat(message.chatId, "${messages.count()} total messages")
     }
 }
